9a43bb2087e6774da024aee11fc013a98b7fbb70
Nesterov-art <shadowwess@gmail.com>
2025-01-10 20:35:33 +0300

    add pyproject.toml

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from src.masks import get_mask_card_number, get_mask_account
+from src.widget import get_date, mask_account_card
+
 card_number = input("Введите номер карты ").strip()
 number_account = input("Введите номер счета ").strip()
+
+if __name__ == "__main__":
+    print(get_date("12.12.2025"))
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 874305"))
+    print("Маска номера счета:", get_mask_account(number_account))
+    print("Маска номера карты:", get_mask_card_number(card_number))
---
+++ src/processing.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+def filter_by_state(operations, state='EXECUTED'):
+    """
+    Фильтрует список операций по значению ключа 'state'.
+    """
+    return [operation for operation in operations if operation.get('state') == state]
+
+
+def sort_by_date(operations, reverse=True):
+    """
+    Сортирует список операций по дате.
+    """
+    return sorted(
+        operations,
+        key=lambda op: datetime.strptime(op['date'], '%Y-%m-%d'),
+        reverse=reverse
+    )
+
+operations = [
+    {'state': 'EXECUTED', 'date': '2025-01-01', 'amount': 100},
+    {'state': 'PENDING', 'date': '2025-01-02', 'amount': 200},
+    {'state': 'EXECUTED', 'date': '2025-01-03', 'amount': 150},
+    {'state': 'CANCELED', 'date': '2025-01-04', 'amount': 50},
+]
+
+executed_operations = filter_by_state(operations)
+print("Фильтрованные операции:", executed_operations)
+
+sorted_operations = sort_by_date(executed_operations)
+print("Отсортированные операции:", sorted_operations)
